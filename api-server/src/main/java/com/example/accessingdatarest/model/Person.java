d6b10a6433c4f0d1e7e9acae68dc808d8145ffef
jbjung <jbjung@koscom.co.kr>
2020-04-07 17:10:40 +0900

    Spring BOOT study resource

---
+++ api-server/src/main/java/com/example/accessingdatarest/model/Person.java
@@ -0,0 +1,23 @@
+package com.example.accessingdatarest.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Person {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String name;
+	
+	private String email;
+	private String phone;
+	private String avatarUrl;
+	private long createdAt;
+}
---
+++ api-server/src/main/java/com/example/accessingdatarest/model/PersonRepository.java
@@ -0,0 +1,10 @@
+package com.example.accessingdatarest.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends CrudRepository<Person, Long> {
+
+
+}
